601dcf7074159dd185bb4242702e78da7f287549
Matt Stout <32655347+Matt-Stout@users.noreply.github.com>
2018-03-08 12:19:49 -0600

    Add files via upload

---
+++ Portfolio.js
@@ -0,0 +1,256 @@
+﻿function overFunction1() {
+    overlay1.style.opacity = 0.4;
+    mask1.style.opacity = 1;
+}
+
+function outFunction1() {
+    overlay1.style.opacity = 1;
+    mask1.style.opacity = 0;
+}
+
+function overFunction2() {
+    overlay2.style.opacity = 0.4;
+    mask2.style.opacity = 1;
+}
+
+function outFunction2() {
+    overlay2.style.opacity = 1;
+    mask2.style.opacity = 0;
+}
+
+function overFunction3() {
+    overlay3.style.opacity = 0.4;
+    mask3.style.opacity = 1;
+}
+
+function outFunction3() {
+    overlay3.style.opacity = 1;
+    mask3.style.opacity = 0;
+}
+
+function overFunction4() {
+    overlay4.style.opacity = 0.4;
+    mask4.style.opacity = 1;
+}
+
+function outFunction4() {
+    overlay4.style.opacity = 1;
+    mask4.style.opacity = 0;
+}
+
+function overFunction5() {
+    overlay5.style.opacity = 0.4;
+    mask5.style.opacity = 1;
+}
+
+function outFunction5() {
+    overlay5.style.opacity = 1;
+    mask5.style.opacity = 0;
+}
+
+function overFunction6() {
+    overlay6.style.opacity = 0.4;
+    mask6.style.opacity = 1;
+}
+
+function outFunction6() {
+    overlay6.style.opacity = 1;
+    mask6.style.opacity = 0;
+}
+
+function overFunction7() {
+    overlay7.style.opacity = 0.4;
+    mask7.style.opacity = 1;
+}
+
+function outFunction7() {
+    overlay7.style.opacity = 1;
+    mask7.style.opacity = 0;
+}
+
+function overFunction8() {
+    overlay8.style.opacity = 0.4;
+    mask8.style.opacity = 1;
+}
+
+function outFunction8() {
+    overlay8.style.opacity = 1;
+    mask8.style.opacity = 0;
+}
+
+function overFunction9() {
+    overlay9.style.opacity = 0.4;
+    mask9.style.opacity = 1;
+}
+
+function outFunction9() {
+    overlay9.style.opacity = 1;
+    mask9.style.opacity = 0;
+}
+
+function overFunction10() {
+    overlay10.style.opacity = 0.4;
+    mask10.style.opacity = 1;
+}
+
+function outFunction10() {
+    overlay10.style.opacity = 1;
+    mask10.style.opacity = 0;
+}
+
+function overFunction11() {
+    overlay11.style.opacity = 0.4;
+    mask11.style.opacity = 1;
+}
+
+function outFunction11() {
+    overlay11.style.opacity = 1;
+    mask11.style.opacity = 0;
+}
+
+function overFunction12() {
+    overlay12.style.opacity = 0.4;
+    mask12.style.opacity = 1;
+}
+
+function outFunction12() {
+    overlay12.style.opacity = 1;
+    mask12.style.opacity = 0;
+}
+
+function overFunction13() {
+    overlay13.style.opacity = 0.4;
+    mask13.style.opacity = 1;
+}
+
+function outFunction13() {
+    overlay13.style.opacity = 1;
+    mask13.style.opacity = 0;
+}
+
+function overFunction14() {
+    overlay14.style.opacity = 0.4;
+    mask14.style.opacity = 1;
+}
+
+function outFunction14() {
+    overlay14.style.opacity = 1;
+    mask14.style.opacity = 0;
+}
+
+function overFunction15() {
+    overlay15.style.opacity = 0.4;
+    mask15.style.opacity = 1;
+}
+
+function outFunction15() {
+    overlay15.style.opacity = 1;
+    mask15.style.opacity = 0;
+}
+
+function overFunction16() {
+    overlay16.style.opacity = 0.4;
+    mask16.style.opacity = 1;
+}
+
+function outFunction16() {
+    overlay16.style.opacity = 1;
+    mask16.style.opacity = 0;
+}
+
+var overlay1 = document.getElementById("overlay1");
+var mask1 = document.getElementById("mask1");
+var overlay2 = document.getElementById("overlay2");
+var mask2 = document.getElementById("mask2");
+var overlay3 = document.getElementById("overlay3");
+var mask3 = document.getElementById("mask3");
+var overlay4 = document.getElementById("overlay4");
+var mask4 = document.getElementById("mask4");
+var overlay5 = document.getElementById("overlay5");
+var mask5 = document.getElementById("mask5");
+var overlay6 = document.getElementById("overlay6");
+var mask6 = document.getElementById("mask6");
+var overlay7 = document.getElementById("overlay7");
+var mask7 = document.getElementById("mask7");
+var overlay8 = document.getElementById("overlay8");
+var mask8 = document.getElementById("mask8");
+var overlay9 = document.getElementById("overlay9");
+var mask9 = document.getElementById("mask9");
+var overlay10 = document.getElementById("overlay10");
+var mask10 = document.getElementById("mask10");
+var overlay11 = document.getElementById("overlay11");
+var mask11 = document.getElementById("mask11");
+var overlay12 = document.getElementById("overlay12");
+var mask12 = document.getElementById("mask12");
+var overlay13 = document.getElementById("overlay13");
+var mask13 = document.getElementById("mask13");
+var overlay14 = document.getElementById("overlay14");
+var mask14 = document.getElementById("mask14");
+var overlay15 = document.getElementById("overlay15");
+var mask15 = document.getElementById("mask15");
+var overlay16 = document.getElementById("overlay16");
+var mask16 = document.getElementById("mask16");
+
+overlay1.addEventListener("mouseover", overFunction1);
+overlay1.addEventListener("mouseout", outFunction1);
+mask1.addEventListener("mouseover", overFunction1);
+
+overlay2.addEventListener("mouseover", overFunction2);
+overlay2.addEventListener("mouseout", outFunction2);
+mask2.addEventListener("mouseover", overFunction2);
+
+overlay3.addEventListener("mouseover", overFunction3);
+overlay3.addEventListener("mouseout", outFunction3);
+mask3.addEventListener("mouseover", overFunction3);
+
+overlay4.addEventListener("mouseover", overFunction4);
+overlay4.addEventListener("mouseout", outFunction4);
+mask4.addEventListener("mouseover", overFunction4);
+
+overlay5.addEventListener("mouseover", overFunction5);
+overlay5.addEventListener("mouseout", outFunction5);
+mask5.addEventListener("mouseover", overFunction5);
+
+overlay6.addEventListener("mouseover", overFunction6);
+overlay6.addEventListener("mouseout", outFunction6);
+mask6.addEventListener("mouseover", overFunction6);
+
+overlay7.addEventListener("mouseover", overFunction7);
+overlay7.addEventListener("mouseout", outFunction7);
+mask7.addEventListener("mouseover", overFunction7);
+
+overlay8.addEventListener("mouseover", overFunction8);
+overlay8.addEventListener("mouseout", outFunction8);
+mask8.addEventListener("mouseover", overFunction8);
+
+overlay9.addEventListener("mouseover", overFunction9);
+overlay9.addEventListener("mouseout", outFunction9);
+mask9.addEventListener("mouseover", overFunction9);
+
+overlay10.addEventListener("mouseover", overFunction10);
+overlay10.addEventListener("mouseout", outFunction10);
+mask10.addEventListener("mouseover", overFunction10);
+
+overlay11.addEventListener("mouseover", overFunction11);
+overlay11.addEventListener("mouseout", outFunction11);
+mask11.addEventListener("mouseover", overFunction11);
+
+overlay12.addEventListener("mouseover", overFunction12);
+overlay12.addEventListener("mouseout", outFunction12);
+mask12.addEventListener("mouseover", overFunction12);
+
+overlay13.addEventListener("mouseover", overFunction13);
+overlay13.addEventListener("mouseout", outFunction13);
+mask13.addEventListener("mouseover", overFunction13);
+
+overlay14.addEventListener("mouseover", overFunction14);
+overlay14.addEventListener("mouseout", outFunction14);
+mask14.addEventListener("mouseover", overFunction14);
+
+overlay15.addEventListener("mouseover", overFunction15);
+overlay15.addEventListener("mouseout", outFunction15);
+mask15.addEventListener("mouseover", overFunction15);
+
+overlay16.addEventListener("mouseover", overFunction16);
+overlay16.addEventListener("mouseout", outFunction16);
+mask16.addEventListener("mouseover", overFunction16);
